7667dc6d69653a9048d21099db5bf0d01b204b8b
MrMcLaine <Lypovskyi@gmail.com>
2023-03-29 19:18:53 +0300

    add little changes in pom.xml & write some cod in MainTest for testing JPA, but so far it works without success

--- src/main/java/ua/magazines/MainTest.java
+++ src/main/java/ua/magazines/MainTest.java
@@ -1,12 +1,32 @@
 package ua.magazines;
 
+import ua.magazines.entity.Role;
 import ua.magazines.entity.User;
-import ua.magazines.service.UserService;
-import ua.magazines.service.impl.UserServiceImpl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class MainTest {
     public static void main(String[] args) {
-        UserService userService = UserServiceImpl.getUserService();
+  /*      UserService userService = UserServiceImpl.getUserService();
         userService.create(new User("Vitaliy", "Kym", "kim@gmail.com", "kimBirthday"));
+*/
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory( "StorePersistence" );
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        User user = new User();
+        user.setFirstName("Test");
+        user.setLastName("TestTest");
+        user.setEmail("test@gmail.com");
+        user.setPassword("test");
+        user.setId(222);
+        user.setRole(Role.USER);
+
+        User user2 = em.find(User.class, 5.0);
+        em.flush();
+
+        em.persist(user);
+        em.getTransaction().commit();
     }
 }
