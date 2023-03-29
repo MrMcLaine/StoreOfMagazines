7ea56d2cee4d9baa5ff25e5b1b555a602bfa6f29
MrMcLaine <Lypovskyi@gmail.com>
2023-03-29 14:36:54 +0300

    Add JPA-functions in MagazineDaoImpl & UserDaoImpl. Also clear all methods in SubscriptionDaoImpl

--- src/main/java/ua/magazines/dao/impl/SubscriptionDaoImpl.java
+++ src/main/java/ua/magazines/dao/impl/SubscriptionDaoImpl.java
@@ -4,48 +4,17 @@ import org.apache.log4j.Logger;
 import ua.magazines.dao.SubscriptionDao;
 import ua.magazines.entity.Subscription;
 
-import java.lang.reflect.InvocationTargetException;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import static ua.magazines.util.ConnectionUtil.openConnection;
 
 public class SubscriptionDaoImpl implements SubscriptionDao {
 
     private static final Logger LOGGER = Logger.getLogger(SubscriptionDaoImpl.class);
-    private static final String READ_ALL = "SELECT * FROM subscription";
-    private static final String CREATE = "INSERT INTO subscription(user_id, magazine_id, status) VALUES (?, ?, ?)";
-    private static final String READ_BY_ID = "SELECT * FROM subscription WHERE id = ?";
-    private static final String UPDATE_BY_ID = "UPDATE subscription SET " +
-                                               "user_id = ?, magazine_id = ?, status = ? WHERE id = ?";
-    private static final String DELETE_BY_ID = "DELETE FROM subscription WHERE id = ?";
-
-    private Connection connection;
-    private PreparedStatement preparedStatement;
-
-    public SubscriptionDaoImpl() {
-        try {
-            this.connection = openConnection();
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException | SQLException e) {
-            LOGGER.error(e);
-        }
-    }
 
     @Override
     public Subscription create(Subscription subscription) {
         try {
-            preparedStatement = connection.prepareStatement(CREATE, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setInt(1, subscription.getUserId());
-            preparedStatement.setInt(2, subscription.getMagazineId());
-            preparedStatement.setBoolean(3, subscription.getStatus());
-            preparedStatement.executeUpdate();
-
-            ResultSet rs = preparedStatement.getGeneratedKeys();
-            rs.next();
-            subscription.setId(rs.getInt(1));
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return subscription;
@@ -54,21 +23,8 @@ public class SubscriptionDaoImpl implements SubscriptionDao {
     @Override
     public Subscription read(Integer id) {
         Subscription subscription = null;
-
         try {
-            preparedStatement = connection.prepareStatement(READ_BY_ID);
-            preparedStatement.setInt(1, id);
-            ResultSet result = preparedStatement.executeQuery();
-            result.next();
-
-            Integer subscriptionId = result.getInt("id");
-            Integer userId = result.getInt("user_id");
-            Integer magazineId = result.getInt("magazine_id");
-            Boolean status = result.getBoolean("status");
-
-            subscription = new Subscription(subscriptionId, userId, magazineId, status);
-
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
 
@@ -78,12 +34,7 @@ public class SubscriptionDaoImpl implements SubscriptionDao {
     @Override
     public Subscription update(Subscription subscription) {
         try {
-            preparedStatement = connection.prepareStatement(UPDATE_BY_ID);
-            preparedStatement.setInt(1, subscription.getUserId());
-            preparedStatement.setInt(2, subscription.getMagazineId());
-            preparedStatement.setBoolean(3, subscription.getStatus());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return subscription;
@@ -92,10 +43,7 @@ public class SubscriptionDaoImpl implements SubscriptionDao {
     @Override
     public void delete(Integer id) {
         try {
-            preparedStatement = connection.prepareStatement(DELETE_BY_ID);
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
     }
@@ -104,18 +52,7 @@ public class SubscriptionDaoImpl implements SubscriptionDao {
     public List<Subscription> readAll() {
         List<Subscription> subscriptionRecords = new ArrayList<>();
         try {
-            preparedStatement = connection.prepareStatement(READ_ALL);
-            ResultSet result = preparedStatement.executeQuery();
-
-            while (result.next()) {
-                Integer subscriptionId = result.getInt("id");
-                Integer userId = result.getInt("user_id");
-                Integer magazineId = result.getInt("magazine_id");
-                Boolean status = result.getBoolean("status");
-
-                subscriptionRecords.add(new Subscription(subscriptionId, userId, magazineId, status));
-            }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return subscriptionRecords;
--- src/main/java/ua/magazines/dao/impl/UserDaoImpl.java
+++ src/main/java/ua/magazines/dao/impl/UserDaoImpl.java
@@ -2,57 +2,29 @@ package ua.magazines.dao.impl;
 
 import org.apache.log4j.Logger;
 import ua.magazines.dao.UserDao;
-import ua.magazines.entity.Role;
 import ua.magazines.entity.User;
+import ua.magazines.shared.FactoryManager;
 
-import java.lang.reflect.InvocationTargetException;
-import java.sql.*;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
-
-import static ua.magazines.util.ConnectionUtil.openConnection;
 
 public class UserDaoImpl implements UserDao {
 
     private static final Logger LOGGER = Logger.getLogger(UserDaoImpl.class);
-    private static final String READ_ALL = "SELECT * FROM user";
-    private static final String CREATE = "INSERT INTO user(first_name, last_name, email, password, role) " +
-                                         "VALUES (?, ?, ?, ?, ?)";
-    private static final String READ_BY_ID = "SELECT * FROM user WHERE id = ?";
-    private static final String READ_BY_EMAIL = "SELECT * FROM user WHERE email = ?";
-    private static final String UPDATE_BY_ID = "UPDATE user SET " +
-                                               "first_name = ?, last_name = ?, email = ?, password = ?, " +
-                                               "role = ? WHERE id = ?";
-    private static final String DELETE_BY_ID = "DELETE FROM user WHERE id = ?";
-
-    private Connection connection;
-    private PreparedStatement preparedStatement;
-
-    public UserDaoImpl() {
-        try {
-            this.connection = openConnection();
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException | SQLException e) {
-            LOGGER.error(e);
-        }
-    }
-
+    private EntityManager em = FactoryManager.getEntityManager();
 
     @Override
     public User create(User user) {
         try {
-            preparedStatement = connection.prepareStatement(CREATE, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setString(3, user.getEmail());
-            preparedStatement.setString(4, user.getPassword());
-            preparedStatement.setString(5, String.valueOf(user.getRole()));
-            preparedStatement.executeUpdate();
-
-            ResultSet rs = preparedStatement.getGeneratedKeys();
-            rs.next();
-            user.setId(rs.getInt(1));
-        } catch (SQLException e) {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return user;
@@ -61,40 +33,18 @@ public class UserDaoImpl implements UserDao {
     @Override
     public User read(Integer id) {
         User user = null;
-
         try {
-            preparedStatement = connection.prepareStatement(READ_BY_ID);
-            preparedStatement.setInt(1, id);
-            ResultSet result = preparedStatement.executeQuery();
-            result.next();
-
-            Integer userId = result.getInt("id");
-            String firstName = result.getString("first_name");
-            String lastName = result.getString("last_name");
-            String email = result.getString("email");
-            String password = result.getString("password");
-            Role role = Role.valueOf(result.getString("role"));
-
-            user = new User(userId, firstName, lastName, email, password, role);
-
-        } catch (SQLException e) {
+            user = em.find(User.class, id);
+        } catch (Exception e) {
             LOGGER.error(e);
         }
-
         return user;
     }
 
     @Override
     public User update(User user) {
         try {
-            preparedStatement = connection.prepareStatement(UPDATE_BY_ID);
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setString(3, user.getEmail());
-            preparedStatement.setString(4, user.getPassword());
-            preparedStatement.setString(5, String.valueOf(user.getRole()));
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return user;
@@ -103,10 +53,7 @@ public class UserDaoImpl implements UserDao {
     @Override
     public void delete(Integer id) {
         try {
-            preparedStatement = connection.prepareStatement(DELETE_BY_ID);
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
     }
@@ -115,20 +62,7 @@ public class UserDaoImpl implements UserDao {
     public List<User> readAll() {
         List<User> userRecords = new ArrayList<>();
         try {
-            preparedStatement = connection.prepareStatement(READ_ALL);
-            ResultSet result = preparedStatement.executeQuery();
-
-            while (result.next()) {
-                Integer userId = result.getInt("id");
-                String firstName = result.getString("first_name");
-                String lastName = result.getString("last_name");
-                String email = result.getString("email");
-                String password = result.getString("password");
-                Role role = Role.valueOf(result.getString("role"));
-
-                userRecords.add(new User(userId, firstName, lastName, email, password, role));
-            }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return userRecords;
@@ -137,22 +71,15 @@ public class UserDaoImpl implements UserDao {
     @Override
     public User getUserByEmail(String email) {
         User user = null;
-
         try {
-            preparedStatement = connection.prepareStatement(READ_BY_EMAIL);
-            preparedStatement.setString(1, email);
-            ResultSet result = preparedStatement.executeQuery();
-            result.next();
-
-            Integer userId = result.getInt("id");
-            String firstName = result.getString("first_name");
-            String lastName = result.getString("last_name");
-            String password = result.getString("password");
-            Role role = Role.valueOf(result.getString("role"));
-
-            user = new User(userId, firstName, lastName, email, password, role);
-
-        } catch (SQLException e) {
+            CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaQuery<User> criteria = builder.createQuery(User.class);
+            Root<User> from = criteria.from(User.class);
+            criteria.select(from);
+            criteria.where(builder.equal(from.get("email"), email));
+            TypedQuery<User> typed = em.createQuery(criteria);
+            user = typed.getSingleResult();
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return user;
